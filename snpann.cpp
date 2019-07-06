59d4dfda0894d3fda3da68d68c78b8fa3d2712ad
Mahul Chakraborty <mahulchak@gmail.com>
2019-07-05 17:44:08 -0700

    completed CDS parsing

--- snpann.cpp
+++ snpann.cpp
@@ -19,15 +19,34 @@ int main()
 	ifstream fin;
 	ofstream fout;
 	fout.open("mycds.txt");
-	string str;
+	string str,seqName,featSeq;
 	vector<string> fields,vs;
-	vector<int> cord(2);//records start and end of a feature
-	map<string,vector<int> > transSpan;//records transcript span
+	
+	vector<int> cord(3);//records start and end and strandedness of a feature
+	map<string,vector<int> > transSpan;//records feature span
+	map<string,string> transChrom;//chromosome of the feature
+	map<string,string> transGene;//records gene name of transcripts
+	map<string,string> seq;//holds genome sequence
+	map<string,string> seqTrans;//extracts transcript sequence
+
 //vector<int> has 3 elements. 1st is start, 2nd is end, 3rd is 0 or 1. 0 is + strand and 1 is - strand
+	fin.open("dmel-all-chromosome-r6.07.1.fasta");//open the genome file
+	while(getline(fin,str))
+	{
+		if(str[0] == '>')//if sequence is found
+		{
+			seqName = str.substr(1);
+		}
+		else
+		{
+			seq[seqName].append(str);
+		}
+	}	
+	fin.close();
 	fin.open("dmel-all-filtered-r6.09.gff");
 	while(getline(fin,str))
 	{
-		if((str.find('#') == string::npos) && (str.find('>') == string::npos))//comments and sequences are ignored
+		if((str.find('#') == string::npos) && (str[0] != '>'))//comments and sequences are ignored
 		{
 			//cout<<"Line found\t"<<str<<endl;
 			fields = fieldSplitter(str);		
@@ -35,17 +54,31 @@ int main()
 			cord[1] = stoi(fields[4]);
 			//cout<<"start=\t"<<fields[3]<<"\t"<<"end=\t"<<fields[4]<<endl;
 		}
+
 		if(str.find("\tCDS") != string::npos)//when the line has CDS feature
 		{
-			fout<<str<<endl;
+			//fout<<str<<endl;
 			vs = parentSplitter(str);
 			for(unsigned int i=0;i<vs.size();i++)
 			{
-				cout<<vs[i]<<'\t'<<cord[0]<<'\t'<<cord[1]<<endl;
+				//cout<<vs[i]<<'\t'<<fields[0]<<'\t'<<cord[0]<<'\t'<<cord[1]<<endl;
+				featSeq = seq[fields[0]].substr(cord[0]-1,cord[1]-cord[0]+1);
+				cout<<vs[i]<<'\t'<<fields[0]<<'\t'<<cord[0]<<'\t'<<cord[1]<<'\t'<<featSeq.size()<<endl;
+				seqTrans[vs[i]].append(featSeq);			
 			}
 		}
-	}	
+		if(str.find("FASTA") != string::npos)
+		{
+			break;
+		}
+	}
 	fin.close();
+	//write the sequence of the CDS
+	for(map<string,string>::iterator it=seqTrans.begin();it!= seqTrans.end();it++)
+	{
+		fout<<">"<<it->first<<endl;
+		fout<<it->second<<endl;
+	}	
 	fout.close();	
 	return 0;
 }
@@ -68,7 +101,7 @@ vector<string> fieldSplitter(string & str)
 			field = str.substr(pos1,pos2-pos1);
 		}
 		pos1 = pos2+1;
-		cout<<field<<endl;
+		//cout<<field<<endl;
 		vs.push_back(field);
 	}
 	return vs;
@@ -100,14 +133,14 @@ vector<string> parentSplitter(string & str)
 				//cout<<"came here 1\t"<<col5<<'\t'<<pos1<<endl;
 				transName = col5.substr(pos1);
 				vs.push_back(transName);
-				cout<<transName<<endl;
+			//	cout<<transName<<endl;
 			}
 			if(pos2 != string::npos)//comma is found
 			{
 			//	cout<<"came here 2"<<endl;
 				transName = col5.substr(pos1,pos2-pos1);	 	
 				vs.push_back(transName);
-				cout<<transName<<endl;
+			//	cout<<transName<<endl;
 			}
 			pos1 = pos2 +1;
 		}
